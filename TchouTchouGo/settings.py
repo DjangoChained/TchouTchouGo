e9d26751decb5e92511f0380c7d2d0335167deb7
Pierant2 <pierre-antoine.waxin@outlook.fr>
2017-10-16 14:35:56 +0200

    Page de connexion

--- TchouTchouGo/settings.py
+++ TchouTchouGo/settings.py
@@ -114,6 +114,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_REDIRECT_URL = '/train/search'
+
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,15 @@
 from django.conf.urls import url
+from django.contrib import admin
 from django.views.generic import RedirectView
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
     url(r'^$', RedirectView.as_view(url='/train/search', permanent=False)),
     url(r'^search$', views.search, name='search'),
+    url(r'^searchResult$', views.searchResult, name='searchResult'),
+    url(r'^tickets$', views.tickets, name='tickets'),
+    url(r'^login$', auth_views.login, name='login'),
+    url(r'^logout$', auth_views.logout, {'next_page': '/train'},
+        name='logout'),
 ]
